63162a07ba0d77d3dfe3dc6d0414043d0aa0677d
Norm Norris <norm@ncnenterprises.com>
2016-03-28 16:18:16 -0700

    Added a version based on the wikipedia pseudocode.

--- SieveOfAtkin.swift
+++ SieveOfAtkin.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class SieveOfAtkin: NSObject {
     
-    func SofA(limit: Int) -> [Bool] {
+    func basic(limit: Int) -> [Bool] {
         let limitSqrt = Int(sqrt(Double(limit)))
         
         var sieveList =  Array(count:limit + 1, repeatedValue:false)
@@ -52,6 +52,99 @@ class SieveOfAtkin: NSObject {
                     }
                 }
              }
+        }
+        
+        return sieveList
+    }
+    
+    func wiki(limit: Int) -> [Bool] {
+        let s = [1, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 49, 53, 59]
+        
+        let limitSqrt = Int(sqrt(Double(limit))) + 1
+        
+        var sieveList =  Array(count:limit + 1, repeatedValue:false)
+        sieveList[0] = false
+        sieveList[1] = false
+        sieveList[2] = true
+        sieveList[3] = true
+        sieveList[4] = false
+        sieveList[5] = true
+        
+        // first quadratic
+        for x in 1 ..< (limitSqrt/2 + 1) {
+            var y = 1
+            y_loop: while y < limitSqrt {
+                let n = (4 * x * x) + (y * y)
+                if n < limit {
+                    switch n % 60 {
+                    case 1, 13, 17, 29, 37, 41, 49, 53:
+                        sieveList[n] = !sieveList[n]
+                    default:
+                        break
+                    }
+                }
+                else {
+                    break y_loop
+                }
+                y += 2
+            }
+        }
+        
+        // second quadratic
+        var x = 1
+        while x < limitSqrt {
+            var y = 2
+            while y < limitSqrt {
+                let n = (3 * x * x) + (y * y)
+                if n < limit {
+                    switch n % 60 {
+                    case 7, 19, 31, 43:
+                        sieveList[n] = !sieveList[n]
+                    default:
+                        break
+                    }
+                }
+                y += 2
+            }
+            x += 2
+        }
+        
+        // third quadratic
+        for x in 2 ..< limitSqrt {
+            var y = x - 1
+            while y > 0 {
+                let n = (3 * x * x) - (y * y)
+                if n < limit {
+                    switch n % 60 {
+                    case 11, 23, 47, 59:
+                        sieveList[n] = !sieveList[n]
+                    default:
+                        break
+                    }
+                }
+                y -= 2
+            }
+        }
+        
+        for w in 0 ..< (limitSqrt/60 + 1) {
+            for i in 0 ..< 16 {
+                let n = 60 * w + s[i]
+                if n >= 7 && n * n <= limit {
+                    if sieveList[n] {
+                        ww_loop: for ww in 0..<(limit/60) {
+                            for j in 0 ..< 16 {
+                                let c = n * n * (60 * ww + s[j])
+                                if c <= limit {
+                                    sieveList[c] = false
+                                }
+                                else {
+                                    break ww_loop
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
         
         return sieveList
